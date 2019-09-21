eb0c3991badab82f98bd1d6d899d461f39c016ed
Merioara Denis <merioara.denis@hotmail.com>
2019-09-21 22:55:17 +0300

    2019-09-21T22:55

---
+++ precache-manifest.33c9b3b49e49c0ca868b94d619375b17.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bd0413e83625a65b727703fe7c99ec9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bde44807fc02f99561dd",
+    "url": "/static/css/main.1e3c36d5.chunk.css"
+  },
+  {
+    "revision": "9be724b7ebcf098534cf",
+    "url": "/static/js/2.4db0ada4.chunk.js"
+  },
+  {
+    "revision": "bde44807fc02f99561dd",
+    "url": "/static/js/main.e71b75b7.chunk.js"
+  },
+  {
+    "revision": "ca5c8fe1c991f76257d4",
+    "url": "/static/js/runtime~main.105f67c5.js"
+  }
+]);
